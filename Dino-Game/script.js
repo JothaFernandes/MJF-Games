6221cc5dc7ee77564ae7990aeebece999e9f06e6
jothaFernandes <jothafernandes@outlook.com.br>
2020-09-21 22:31:03 -0300

    editando os arquivos de dino

--- Dino-Game/script.js
+++ Dino-Game/script.js
@@ -31,31 +31,33 @@ function jump(){
             position += 20;
             dino.style.bottom = position +'px';
         }
-    }, 20);
+    }, 30);
 }
 
 function createcactus(){
     const cactus = document.createElement('div');
-    let cactusposition = 1300;
-    let randomTime = Math.random() * 6000;
+    let cactusposition = 840;
+    let randomTime = 800 + Math.floor(Math.random() * 3000);
 
     cactus.classList.add('cactus');
-    cactus.style.left = 1300 + 'px';
+    cactus.style.left = 840 + 'px';
     background.appendChild(cactus);
 
     let leftInterval = setInterval(() => {
-        if(cactusposition < - 60){
+        if(cactusposition <  0){
             clearInterval(leftInterval);
             background.removeChild(cactus);
         }else if(cactusposition > 0 && cactusposition < 60 && position < 60){
             clearInterval(leftInterval);
-            document.body.innerHTML = '<h1 class="game-over">Fim de jogo</h1>';
+            document.body.innerHTML = '<h1 class="game-over"></h1>';
         }
         else {
         cactusposition -= 10;
         cactus.style.left = cactusposition + 'px';
+        score += 1;
+        document.getElementById('score').innerHTML = score;
         }
-    }, 20);
+    }, 30);
 
     setTimeout(createcactus,randomTime);
 }
